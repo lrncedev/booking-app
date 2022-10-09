328c18e8e63a882e95050a4649a62f4d03efd5c2
Laurence Cadag <laurencecads1@gmail.com>
2022-10-09 11:35:13 +0800

    Commit 6: Added models

---
+++ models/listing.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const listingSchema = new mongoose.Schema({
+  listingName: {
+    type: String,
+    required: true
+  },
+  userName:  {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  datePosted: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+})
+
+module.exports = mongoose.model('Listing', listingSchema);
